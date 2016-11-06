ac0cc018b5efd6e0770a0c2cd3fcaee60a43daea
Andrew <a.balanyuk@yahoo.com>
2016-11-06 23:10:02 +0200

    Added todos reducers and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var moment = require('moment');
+var uuid = require('node-uuid');
+
 export var searchTextReducer = (state = '', action) => {
   switch(action.type){
     case 'SET_SEARCH_TEXT':
@@ -7,11 +10,39 @@ export var searchTextReducer = (state = '', action) => {
   }
 };
 
-// showCompleted
 export var showCompletedReducer = (state = false, action) => {
   switch(action.type){
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
+    default:
+      return state;
+  }
+};
+
+export var todosReducer = (state = [], action) => {
+  switch(action.type){
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id === action.id){
+          return {
+            ...todo,
+            completed: !todo.completed,
+            completedAt: !todo.completed ? moment().unix() : undefined
+          };
+        } else
+        return todo;
+      });
     default:
       return state;
   }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,7 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
+var moment = require('moment');
+var uuid = require('node-uuid');
 
 var reducers = require('reducers');
 
@@ -24,6 +26,45 @@ describe('Reducers', () => {
       var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Call HER?'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: uuid(),
+          text: 'you can run around',
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        },
+        {
+          id: uuid(),
+          text: 'even put me down',
+          completed: true,
+          createdAt: moment().unix(),
+          completedAt: moment().unix()
+        }
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: todos[0].id
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res.length).toEqual(2);
+      expect(res[0].completed).toEqual(!todos[0].completed);
+      expect(res[0].completedAt).toNotEqual(undefined);
     });
   });
 });
