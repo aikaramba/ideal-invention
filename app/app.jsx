06de34e2ea0f1fcc1afe3faca7e8465715419316
Andrew <a.balanyuk@yahoo.com>
2016-11-08 21:45:22 +0200

    Added React router middleware and redirect on auth

--- app/app.jsx
+++ app/app.jsx
@@ -1,15 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
 // react components
-import Login from 'Login';
-import TodoApp from 'TodoApp';
+import router from 'app/router/';
 // redux
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
+import firebase from 'app/firebase/';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    hashHistory.push('/todos');
+  } else {
+    hashHistory.push('/');
+  }
+});
 
 store.dispatch(actions.startAddTodos());
 
@@ -22,12 +29,7 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <IndexRoute component={Login} />
-        <Route path = "todos" component={TodoApp}/>
-      </Route>
-    </Router>
+    {router}
   </Provider>,
   document.getElementById('app')
 );
